871fa051c70d6c0889f8aa5b329139c007b12037
Abdul Qader Waddah <aboodwaddah516@gmail.com>
2026-03-14 15:19:10 +0200

    refactor: wire up new routes in app.js and remove unused utils

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { errorMiddleware } from './middlewares/errorMiddleware.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/auth/org', authRoutes);
+
+
+// auth routes
+app.use('/api/auth', authRoutes);
+
+
+
+
+
+// users routes
+app.use("/api/users", userRoutes);
+
+
+
+
+
 
 app.use(errorMiddleware);
 
